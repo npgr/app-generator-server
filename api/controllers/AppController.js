1690c25d7eac8a2d6ce72073935b6024c504c76d
Nuno <e_npgr@yahoo.com>
2016-02-02 17:45:15 -0430

    AppController.deleteApp: Not ready yet

--- api/controllers/AppController.js
+++ api/controllers/AppController.js
@@ -11,6 +11,50 @@ module.exports = {
 		res.locals.user = {user: req.session.user, name: req.session.username}
 		res.locals.data = []
 		res.view("app/list")
+	},
+	deleteApp : function (req, res) {
+		var app_id = req.param('app')
+		App.detroy({id: app_id})
+		  .exec(function(err, app_deleted) {
+			Model.destroy({app: app_deleted.id})
+			  .exec(function(err, models) {
+				console.log('model deleted: ', models )
+				for (j=0; j< models.length; j++)
+				  Attribute.destroy({model: models[j]})
+					.exec(function(err, attrs) {
+					
+					})  
+			})
+			ModelFunction.destroy({app: app_deleted.id})
+			  .exec(function(err, mfuncs) {
+				console.log('mfunction deleted: ', mfuncs)
+				for (i=0; i < mfuncs.length; i++)
+				{
+					FunctionList.destroy({mfunction: mfuncs[i]})
+					  .exec(function(err, f_list) {
+						console.log('Function List deleted: ', f_list)
+					})
+					
+				}
+			})	
+		})
+		/*Model.destroy({app: app})
+		  exec(function(err, models) { 
+			for (i=0; i < models.length; i++)
+			{
+				ModelFunction.find({model: models[i].id})
+				  exec(function(err, mfuncs) {
+					for (j=0; j < mfuncs.length; j++)
+						ModelFunction.delete({id: mfuncs[i].id})
+				})
+				FunctionList.find({model: models[i].id})
+				  exec(function(err, funclist) {
+					for (j=0; j < funclist.length; j++)
+						FunctionList.delete({id: })
+				})
+				Model.delete({id: models[i].id})
+			}
+		})*/
 	}
 };
 
