0014ac6ddb5ae3ab6c72789159ba7bfabd027e25
Nuno <e_npgr@yahoo.com>
2016-03-02 16:45:48 -0430

    move deploy.sh to root folder

--- deploy.sh
+++ deploy.sh
@@ -17,12 +17,14 @@ find deploy/node_modules -iname 'changelog' -exec rm {} \;
 find deploy/node_modules -iname 'readme*' -exec rm {} \;
 
 # Compact all .js files
-# node_modules
-find node_modules/winston -iname '*.js' -exec uglifyjs --compress --mangle -o {} -- {} \;
+# node_modules -> it takes about 1 hour
+find deploy/node_modules -iname '*.js' -exec uglifyjs --compress --mangle -o {} -- {} \;
+
+# remove assets/components/ not used
+
+# delete from Controllers the generate options
 
 
 # Vulcanize
 
 # vulcanize --inline-css --inline-scripts reporte.html > reporte2.html
-
-
